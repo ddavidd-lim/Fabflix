49b18d03058abbe164b9102534743087e886ef34
David <jxl1238@gmail.com>
2023-10-25 13:43:41 -0700

    Added filter, login servlet and User class

---
+++ src/LoginServlet.java
@@ -0,0 +1,44 @@
+import com.google.gson.JsonObject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        /* This example only allows username/password to be test/test
+        /  in the real project, you should talk to the database to verify username/password
+        */
+        JsonObject responseJsonObject = new JsonObject();
+        if (username.equals("anteater") && password.equals("123456")) {
+            // Login success:
+
+            // set this user into the session
+            request.getSession().setAttribute("user", new User(username));
+
+            responseJsonObject.addProperty("status", "success");
+            responseJsonObject.addProperty("message", "success");
+
+        } else {
+            // Login fail
+            responseJsonObject.addProperty("status", "fail");
+            // Log to localhost log
+            request.getServletContext().log("Login failed");
+            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
+            if (!username.equals("anteater")) {
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            } else {
+                responseJsonObject.addProperty("message", "incorrect password");
+            }
+        }
+        response.getWriter().write(responseJsonObject.toString());
+    }
+}

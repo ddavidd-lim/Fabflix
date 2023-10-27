7b6826636f25df2a5716a8cb3cd0cdc185cecb37
Caitlynn C. <caitlypc@uci.edu>
2023-10-27 12:45:01 -0700

    started on servlets for main page and results

---
+++ src/SearchServlet.java
@@ -0,0 +1,31 @@
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+@WebServlet(name = "SearchServlet", urlPatterns = "/results")
+
+public class SearchServlet extends HttpServlet{
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+    }
+
+}

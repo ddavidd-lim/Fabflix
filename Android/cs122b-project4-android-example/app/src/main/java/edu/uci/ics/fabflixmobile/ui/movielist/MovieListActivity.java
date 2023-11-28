a416562adb72e68446ddb0fde2b15cc023350e81
David <jxl1238@gmail.com>
2023-11-27 20:49:16 -0800

    Completed SingleMovie Display and clicking

--- Android/cs122b-project4-android-example/app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListActivity.java
+++ Android/cs122b-project4-android-example/app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListActivity.java
@@ -3,15 +3,24 @@ package edu.uci.ics.fabflixmobile.ui.movielist;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
 import edu.uci.ics.fabflixmobile.R;
+import edu.uci.ics.fabflixmobile.data.NetworkManager;
 import edu.uci.ics.fabflixmobile.data.model.Movie;
+import edu.uci.ics.fabflixmobile.ui.search.SearchActivity;
+import edu.uci.ics.fabflixmobile.ui.singlemovie.SingleMovieActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,7 +29,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MovieListActivity extends AppCompatActivity {
+//    private EditText movieid;
 
+    /*
+      In Android, localhost is the address of the device or the emulator.
+      To connect to your machine, you need to use the below IP address
+     */
+    private final String host = "10.0.2.2";
+    private final String port = "8080";
+    private final String domain = "Fabflix_war";
+    private final String baseURL = "http://" + host + ":" + port + "/" + domain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +54,8 @@ public class MovieListActivity extends AppCompatActivity {
                 // For example, you can parse it into a JSONArray or JSONObject.
                 try {
                     JSONArray moviesJSONArray = new JSONArray(moviesJSONString);
-                    // Now you can work with the JSON array
-
-                    // Example: iterating through the array
                     for (int i = 0; i < moviesJSONArray.length(); i++) {
-                        // Get individual JSON objects from the array
                         JSONObject movieObject = moviesJSONArray.getJSONObject(i);
-
-                        // Access properties of each movie object
                         String movieId = movieObject.getString("movie_id");
                         String title = movieObject.getString("movie_title");
                         String director = movieObject.getString("movie_director");
@@ -63,11 +75,7 @@ public class MovieListActivity extends AppCompatActivity {
                                 starNameArray.add(idName[1]);
                             }
                         }
-
-//                        Log.d("genres: ", top3genres);
                         ArrayList<String> genreNameArray = new ArrayList<>(Arrays.asList(top3genres.split(",")));
-                        // Do something with the movie data
-                        // For instance, add it to a list, display it, etc.
                         movies.add(new Movie(movieId, title, director, year, starNameArray, genreNameArray, rating));
                         Log.d("movie_id: ", movieId);
                         Log.d("title: ", title);
@@ -89,6 +97,29 @@ public class MovieListActivity extends AppCompatActivity {
             Movie movie = movies.get(position);
             @SuppressLint("DefaultLocale") String message = String.format("Clicked on position: %d, name: %s, %d", position, movie.getTitle(), movie.getYear());
             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            final RequestQueue queue = NetworkManager.sharedManager(this).queue;
+            // request type is POST
+            final StringRequest movieRequest = new StringRequest(
+                    Request.Method.GET,
+                    baseURL + "/api/single-movie?id=" + movie.getMovieId(),
+                    response -> {
+                        // TODO: pass id to SingleMovieServlet and parse response
+                        Log.d("Selection.success", response);
+
+                        // initialize the activity(page)/destination
+                        Intent SingleMoviePage = new Intent(MovieListActivity.this, SingleMovieActivity.class);
+                        SingleMoviePage.putExtra("singlemovie", response);
+                        // activate the list page.
+                        startActivity(SingleMoviePage);
+
+                    },
+                    error -> {
+                        // error
+                        Log.d("Selection.error", error.toString());
+                    });
+            movieRequest.setRetryPolicy(new DefaultRetryPolicy(120000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            // important: queue.add is where the login request is actually sent
+            queue.add(movieRequest);
         });
     }
 }
